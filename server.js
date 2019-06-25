96dd5de2563de4d991558ab0ecd034c5ddf4f975
huhn <sehe89@gmail.com>
2019-06-25 17:38:10 +0200

    add github pages to allowd origins

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const httpsServer = https.createServer(credentials, app);
 
 var io = require('socket.io');
 
-var allowedOrigins = "http://localhost:* http://127.0.0.1:*";
+var allowedOrigins = "http://localhost:* http://127.0.0.1:* https://machineeconomy.github.io:*";
 var socket_path = '/socket';
 var cors = require('cors')
 
