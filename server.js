9f19fd0a8771c52ef5fe9107a9bb925309119e6b
huhn <sehe89@gmail.com>
2019-07-22 00:44:07 +0200

    refactore scripts

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ const path = require('path');
 
 const { router } = require('./src/WebServer.js')
 const { socketServer } = require('./src/WebSockets.js')
-const { fetchAndBroadcastBalanceFrom, handleOrder, getCurrentAddress} = require('./src/WebTangle.js')
+const { fetchAndBroadcastBalanceFrom, handleOrder, getCurrentAddress } = require('./src/WebTangle.js')
 const { log } = require('./src/Logger.js')
-const { getCurrentBalance, getCurrentIndex  } = require('./src/Database.js')
+const { runJavascript, runPython, runShell, runRust } = require('./src/ScriptHandler.js')
+const { getCurrentBalance, getCurrentIndex } = require('./src/Database.js')
 
 const NAME = process.env.NAME
 const IS_PROVIDER = process.env.IS_PROVIDER
 const PROVIDER_URL = process.env.PROVIDER_URL
-
-const sh = require('shelljs');
-const ps = require('python-shell');
-const js = require('./hello_scripts/hello_javascript.js');
 
 
 router.get('/', (req, res) => res.sendFile(path.join(__dirname + '/frontend/index.html')));
@@ -28,39 +25,37 @@ router.post('/orders', function (request, response) {
 });
 
 router.post('/hello_shell', function (request, response) {
-
-    const { stdout, stderr, code } = sh.exec('./hello_scripts/hello_shell.sh', { silent: true })
-    console.log("stdout", stdout)
-    console.log("stderr", stderr)
-    console.log("code", code)
-
-    response.send(stdout)
-});
-
-router.post('/hello_python', function (request, response) {
-
-    ps.PythonShell.run('.hello_scripts/hello_python.py', {}, function (err, results) {
-        if (err) throw err;
-        // results is an array consisting of messages collected during execution
-        console.log('results: %j', results);
-        response.send(results[0])
-
+    runShell().then((result) => {
+        response.send(result)
+    }, (error) => {
+        console.log("err", error)
+        response.send(error)
     });
 });
 
-router.post('/hello_javascript', function (request, response) {
-    let result = js.hello_javascript();
-    response.send(result)
+router.post('/hello_python', function (request, response) {
+    runPython().then((result) => {
+        response.send(result)
+    }, (error) => {
+        response.send(error)
+    });
 
 });
 
-router.post('/hello_rust', function (request, response) {
-    const { stdout, stderr, code } = sh.exec('./hello_scripts/hello_rust', { silent: true })
-    console.log("stdout", stdout)
-    console.log("stderr", stderr)
-    console.log("code", code)
+router.post('/hello_javascript', function (request, response) {
+    runJavascript().then((result) => {
+        response.send(result)
+    }, (error) => {
+        response.send(error)
+    });
+});
 
-    response.send(stdout)
+router.post('/hello_rust', function (request, response) {
+    runRust().then((result) => {
+        response.send(result)
+    }, (error) => {
+        response.send(error)
+    });
 });
 
 socketServer.on('connection', function (socket) {
---
+++ src/ScriptHandler.js
@@ -0,0 +1,50 @@
+const ps = require('python-shell');
+const sh = require('shelljs');
+
+const runJavascript = function () {
+    return new Promise((resolve, reject) => {
+
+        const js = require('../hello_scripts/hello_javascript.js');
+
+        resolve(js.hello_javascript())
+
+    });
+}
+
+const runPython = function () {
+    return new Promise((resolve, reject) => {
+        ps.PythonShell.run('./hello_scripts/hello_python.py', {}, (err, results) => {
+            if (err) reject(err);
+            resolve(results[0])
+        });
+    })
+}
+
+const runShell = function () {
+    return new Promise((resolve, reject) => {
+        const { stdout, stderr, code } = sh.exec('./hello_scripts/hello_shell.sh', { silent: true })
+        console.log("stdout", stdout)
+        console.log("stderr", stderr)
+        console.log("code", code)
+
+        resolve(stdout)
+    })
+}
+
+const runRust = function () {
+    return new Promise((resolve, reject) => {
+        const { stdout, stderr, code } = sh.exec('./hello_scripts/hello_rust', { silent: true })
+        console.log("stdout", stdout)
+        console.log("stderr", stderr)
+        console.log("code", code)
+
+        resolve(stdout)
+    })
+}
+
+module.exports = {
+    runJavascript,
+    runPython,
+    runShell,
+    runRust
+}
