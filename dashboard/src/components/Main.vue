9705fc91145505447839a757e87245124dbde227
huhn <sehe89@gmail.com>
2019-08-18 01:57:33 +0200

    add particles

--- dashboard/src/components/Main.vue
+++ dashboard/src/components/Main.vue
@@ -1,6 +1,8 @@
 <template>
   <main class="main">
+      
     <div class="circle">
+     
       <div class="circle--inner"></div>
       <div class="circle--outer"></div>
       <div
@@ -10,10 +12,14 @@
         <span class="status-text">Working</span>
       </div>
       <div class="circle--info-box">
-          <span class="label">Balance</span>
-          <span class="value">12.120.301</span>
-          <span class="label">IOTA</span>
+        <span class="label">Balance</span>
+        <span class="value">12.120.301</span>
+        <span class="label">IOTA</span>
       </div>
+       <div class="wrap">
+        <div class="c" v-for="i in 100" :key="i"></div>
+      </div>
+
     </div>
   </main>
 </template>
@@ -33,57 +39,117 @@ export default {};
     position: relative;
     height: 536px;
     width: 536px;
-    background-color: var(--primary);
+    border: 50px solid var(--primary);
     border-radius: 50%;
+    box-sizing: border-box;
     display: flex;
     justify-content: center;
     align-items: center;
     &--inner {
-        margin: 40px;
-        height: calc(100% - 80px);
-        width: 100%;
-        border-radius: 50%;
-        background-color: var(--black);
+      margin: 40px;
+      height: calc(100% - 80px);
+      width: 100%;
+      border-radius: 50%;
+      background-color: transparent;
     }
     &--outer {
     }
     &--status-box {
-        position: absolute;
-        bottom: -15px;
-        z-index: 3;
-        height: 94px;
-        width: 243px;
-        background-repeat: no-repeat;
-        background-size: cover;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        .status-text {
-            font-family: "Oswald", sans-serif;
-            text-transform: uppercase;
-            letter-spacing: 0.1em;
-            font-size: 20px;
-            margin-bottom: 5px;
-        }
+      position: absolute;
+      bottom: -65px;
+      z-index: 3;
+      height: 94px;
+      width: 243px;
+      background-repeat: no-repeat;
+      background-size: cover;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .status-text {
+        font-family: "Oswald", sans-serif;
+        text-transform: uppercase;
+        letter-spacing: 0.1em;
+        font-size: 20px;
+        margin-bottom: 5px;
+      }
     }
     &--info-box {
-        position: absolute;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        span {
-            font-family: "Oswald", sans-serif;
-        }
-        .value {
-            font-size: 46px;
-        }
-        .label {
-            text-transform: uppercase;
-            letter-spacing: 0.05em;
-            opacity: .5;
-        }
+      position: absolute;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      span {
+        font-family: "Oswald", sans-serif;
+      }
+      .value {
+        font-size: 46px;
+      }
+      .label {
+        text-transform: uppercase;
+        letter-spacing: 0.05em;
+        opacity: 0.5;
+      }
+    }
+  }
 
+  // best in chrome
+  $total: 100; // total particles
+  $orb-size: 200px;
+  $particle-size: 2px;
+  $time: 50s;
+  $base-hue: 180; // change for diff colors (180 is nice)
+
+  .wrap {
+      position: absolute;
+    width: 0;
+    height: 0;
+    transform-style: preserve-3d;
+    perspective: 1000px;
+    animation: rotate $time infinite linear; // rotate orb
+  }
+
+  @keyframes rotate {
+    100% {
+      transform: rotateY(360deg) rotateX(360deg);
+    }
+  }
+
+  .c {
+    position: absolute;
+    width: $particle-size;
+    height: $particle-size;
+    border-radius: 50%;
+    opacity: 0;
+  }
+
+  @for $i from 1 through $total {
+    $z: (random(360) * 1deg); // random angle to rotateZ
+    $y: (random(360) * 1deg); // random to rotateX
+    $hue: ((40 / $total * $i) + $base-hue); // set hue
+
+    .c:nth-child(#{$i}) {
+      // grab the nth particle
+      animation: orbit#{$i} $time infinite;
+      animation-delay: ($i * 0.01s);
+      background-color: hsla($hue, 100%, 50%, 1);
+    }
+
+    @keyframes orbit#{$i} {
+      20% {
+        opacity: 1; // fade in
+      }
+      30% {
+        transform: rotateZ(-$z) rotateY($y) translateX($orb-size) rotateZ($z); // form orb
+      }
+      80% {
+        transform: rotateZ(-$z) rotateY($y) translateX($orb-size) rotateZ($z); // hold orb state 30-80
+        opacity: 1; // hold opacity 20-80
+      }
+      100% {
+        transform: rotateZ(-$z) rotateY($y) translateX(($orb-size * 3))
+          rotateZ($z); // translateX * 3
+      }
     }
   }
 }
