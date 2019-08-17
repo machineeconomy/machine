81f8fe0ece6b2e57736fb152c14c0be06ce5ff82
huhn <sehe89@gmail.com>
2019-08-17 21:56:57 +0200

    add dashboard vue app

---
+++ dashboard/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Dashboard/>
+  </div>
+</template>
+
+<script>
+import Dashboard from './components/Dashboard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Dashboard
+  }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
