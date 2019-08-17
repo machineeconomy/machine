68701b4f5f4cac221ce9266cfd8292cb3a152a9c
huhn <sehe89@gmail.com>
2019-08-17 22:13:01 +0200

    setup styling

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,22 +1,62 @@
 <template>
   <div id="app">
-    <Dashboard/>
+    <Dashboard />
   </div>
 </template>
 
 <script>
-import Dashboard from './components/Dashboard.vue'
+import Dashboard from "./components/Dashboard.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Dashboard
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Open+Sans|Oswald:700&display=swap");
+
+:root {
+  --black: #171717;
+  --dark: #15272e;
+  --primary: #00b0f0;
+  --light: #efefef;
+  --white: #ffffff;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  
+  background-color: var(--black);
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  * {
+    color: var(--white);
+  }
+}
+
+h1,
+h2,
+h3,
+h4 {
+  margin: 0;
+  font-weight: 700;
+  font-family: "Oswald", sans-serif;
+}
+
+h1 {
+  font-size: 46px;
+}
+
+p,
+span {
+  font-size: 16px;
+  font-family: "Open Sans", sans-serif;
 }
 </style>
